6b8ebf4c816839032fd4fca0915f192a35de30c7
John Hooks <hooksie11@gmail.com>
2024-04-05 20:58:03 -0400

    split requests out into goroutines

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -72,6 +72,10 @@ func (e *Exporter) WatchForServices(interval int) {
 			defer mu.Unlock()
 			var info micro.Info
 
+			if !json.Valid(m.Data) {
+				return
+			}
+
 			if err := json.Unmarshal(m.Data, &info); err != nil {
 				logr.Error(err)
 				return
@@ -126,42 +130,54 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 
 // scrape gathers all metrics for each service found
 func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
-	for _, v := range e.services {
-		var stats micro.Stats
-		subject := fmt.Sprintf("%s.%s.%s.%s", micro.APIPrefix, micro.StatsVerb, v.Name, v.ID)
-		resp, err := e.nc.Request(subject, nil, 1*time.Second)
-		if err != nil && err != nats.ErrNoResponders {
-			logr.Error(err)
-			continue
-		}
-
-		if err == nats.ErrNoResponders {
-			delete(e.services, v.ID)
-			continue
-		}
-
-		if err := json.Unmarshal(resp.Data, &stats); err != nil {
-			logr.Error(err)
-			continue
-		}
-
-		for _, endpoint := range stats.Endpoints {
-			labels := fmt.Sprintf("%s_%s_%s", stats.Name, stats.ID, endpoint.Name)
-			for _, metric := range e.metrics {
-				switch metric.valueType {
-				case totalRequests:
-					ch <- prometheus.MustNewConstMetric(metric.desc, prometheus.CounterValue, float64(endpoint.NumRequests), labels)
-				case totalErrors:
-					ch <- prometheus.MustNewConstMetric(metric.desc, prometheus.CounterValue, float64(endpoint.NumErrors), labels)
-				case totalProcessingTime:
-					ch <- prometheus.MustNewConstMetric(metric.desc, prometheus.CounterValue, float64(endpoint.ProcessingTime.Seconds()), labels)
-				case averageProcessingTime:
-					ch <- prometheus.MustNewConstMetric(metric.desc, prometheus.CounterValue, float64(endpoint.AverageProcessingTime.Milliseconds()), labels)
-
-				}
-			}
-
-		}
-
+	var wg sync.WaitGroup
+	wg.Add(len(e.services))
+	for _, service := range e.services {
+		go e.scrapeService(&wg, service, ch)
 	}
+	wg.Wait()
+}
+
+func (e *Exporter) scrapeService(wg *sync.WaitGroup, service micro.Info, ch chan<- prometheus.Metric) {
+	defer wg.Done()
+	var stats micro.Stats
+	subject := fmt.Sprintf("%s.%s.%s.%s", micro.APIPrefix, micro.StatsVerb, service.Name, service.ID)
+	resp, err := e.nc.Request(subject, nil, 1*time.Second)
+	if err != nil && err != nats.ErrNoResponders {
+		logr.Error(err)
+		return
+	}
+
+	if err == nats.ErrNoResponders {
+		delete(e.services, service.ID)
+		return
+	}
+
+	if err := json.Unmarshal(resp.Data, &stats); err != nil {
+		logr.Error(err)
+		return
+	}
+	wg.Add(len(stats.Endpoints))
+	for _, endpoint := range stats.Endpoints {
+		go e.scrapeStats(wg, stats, endpoint, ch)
+	}
+}
+
+func (e *Exporter) scrapeStats(wg *sync.WaitGroup, stats micro.Stats, endpoint *micro.EndpointStats, ch chan<- prometheus.Metric) {
+	defer wg.Done()
+	labels := fmt.Sprintf("%s_%s_%s", stats.Name, stats.ID, endpoint.Name)
+	for _, metric := range e.metrics {
+		switch metric.valueType {
+		case totalRequests:
+			ch <- prometheus.MustNewConstMetric(metric.desc, prometheus.CounterValue, float64(endpoint.NumRequests), labels)
+		case totalErrors:
+			ch <- prometheus.MustNewConstMetric(metric.desc, prometheus.CounterValue, float64(endpoint.NumErrors), labels)
+		case totalProcessingTime:
+			ch <- prometheus.MustNewConstMetric(metric.desc, prometheus.CounterValue, float64(endpoint.ProcessingTime.Seconds()), labels)
+		case averageProcessingTime:
+			ch <- prometheus.MustNewConstMetric(metric.desc, prometheus.CounterValue, float64(endpoint.AverageProcessingTime.Milliseconds()), labels)
+
+		}
+	}
+
 }
